04666366de53240950139ecbcdb65a09f9046e6f
Mouez-HJ <moezhajria@gmail.com>
2016-10-07 17:06:13 +0200

    client serveur problem s.send(message)

--- TcpServer.py
+++ TcpServer.py
@@ -1,28 +1,27 @@
-
 import socket
+
 def Main():
-    host ='127.0.0.1'
+    host = '127.0.0.1'
     port = 5000
 
     s = socket.socket()
-    s.bind((host, port))
+    s.bind((host,port))
 
     s.listen(1)
     c, addr = s.accept()
-    
+    print ("Connection from: " + str(addr))
     while True:
-            data = c.recv(1024)
-            if not data:
-                   break
-           
-            data = str(data).upper()
-           
-            c.send(data)
+        print ("from connected user: " + str(data))
+        data = c.recv(1024)
+        if not data:
+            break
+        
+        
+        data = str(data).upper()
+        print ("sending: " + str(data))
+        byte_message = data.encode()
+        c.send(byte_message)
     c.close()
+
 if __name__ == '__main__':
-            Main()
-            
-
-
-     
-            
+    Main()
